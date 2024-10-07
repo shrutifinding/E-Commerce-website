8743f1490241032b1016573ee7f7560a6d256359
shrutifinding <163763937+shrutifinding@users.noreply.github.com>
2024-10-07 14:50:08 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE products (
 );
 
 INSERT INTO products (name, price, image) VALUES
-('Product 1', 20, 'product1.jpg'),
-('Product 2', 30, 'product2.jpg'),
-('Product 3', 80, 'product3.jpg');
+('Product 1', 20, 'https://tse3.mm.bing.net/th?id=OIP.ZUktNr4TbMEsqzJ62haeSgHaHa&pid=Api&P=0&h=180'),
+('Product 2', 30, 'https://tse4.mm.bing.net/th?id=OIP.T3tbPj9n4-z5QSZDQH2tiQHaKn&pid=Api&P=0&h=180'),
+('Product 3', 80, 'https://tse4.mm.bing.net/th?id=OIP.uk4HvdXE1uEQ9dw4z0QNtQHaHa&pid=Api&P=0&h=180');
  
