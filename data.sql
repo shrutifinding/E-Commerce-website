cfd5e57f5d9cbd957cf7386cf794b16df0300494
shrutifinding <163763937+shrutifinding@users.noreply.github.com>
2024-10-07 18:10:51 +0530

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,7 +1,7 @@
 CREATE TABLE products (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
+    price INT(10) NOT NULL,
     image VARCHAR(255) NOT NULL
 );
 
